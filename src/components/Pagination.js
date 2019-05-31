0766fdc3ccd1d1e88c27de07788e9fc6d8499450
Sabrina Markon <phpsitescripts@outlook.com>
2019-05-31 06:28:49 -0600

    Render the pagination blocks.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -85,8 +85,46 @@ export default class Pagination extends Component {
   }
 
   render() {
+    if (!this.totalRecords || this.totalPages === 1) {
+      return null;
+    }
+    const {currentPage} = this.state;
+    const pages = this.fetchPageNumbers();
     return (
-      <div></div>
+      <Fragment>
+        <nav aria-label="Meteorite Database Pagination">
+          <ul className="pagination">
+            { pages.map((page, index) => {
+              if (page === LEFT_PAGE) {
+                return (
+                  <li key={index} className="page-item">
+                  <a className="page-link" href="#" aria-label="Previous" onClick={this.handleMoveLeft}>
+                    <span aria-hidden="true">&raquo;</span>
+                    <span className="sr-only">Previous</span>
+                  </a>
+                </li>
+                );
+              }
+              if (page === RIGHT_PAGE) {
+                return (
+                  <li key={index} className="page-item">
+                  <a className="page-link" href="#" aria-label="Next" onClick={this.handleMoveRight}>
+                    <span aria-hidden="true">&raquo;</span>
+                    <span className="sr-only">Next</span>
+                  </a>
+                </li>
+                );
+              }
+
+              return (
+                <li key={index} className={`page-item${ currentPage === page ? ' active' : ''}`}>
+                  <a className="page-link" href="#" onClick={ this.handleClick(page) }>{ page }</a>
+                </li>   
+              );
+            })}
+          </ul>
+        </nav>
+      </Fragment>
     );
   }
 }

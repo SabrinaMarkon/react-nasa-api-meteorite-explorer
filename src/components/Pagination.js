5d68e1ea8bf782e3b8387f03307da1aece2449e4
Sabrina Markon <phpsitescripts@outlook.com>
2019-05-31 03:54:17 -0600

    Create Pagination component.

---
+++ src/components/Pagination.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Pagination {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 1
+    }
+    
+  }
+}

976043c82bdab4d64087c5f935c7e9f54b8ab84a
Sabrina Markon <phpsitescripts@outlook.com>
2019-05-31 05:15:52 -0600

    Create instance variables in constructor for Pagination component.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 export default class Pagination extends Component {
 
@@ -7,7 +8,26 @@ export default class Pagination extends Component {
     this.state = {
       currentPage: 1
     }
-
+    const props = {
+      totalRecords: null, 
+      pageLimit: 100, 
+      pageNeighbors = 0
+    }
+    this.pageLimit = typeof pageLimit === 'number' ? pageLimit: 100;
+    this.totalRecords = typeof totalRecords === 'number' ? totalRecords : 0;
+    /* pageNeighbors means the page numbers on either side of the current page.
+    The minimum value is 0 and the maximum value is 2. If not specified, 
+    it defaults to 0 as defined in the constructor(). */
+    this.pageNeighbors = typeof pageNeighbors === 'number'
+      ? Math.max(0, Math.min(pageNeighbors, 2))
+      : 0;
+    this.totalPages = Math.ceil(this.totalRecords / this.pageLimit);
   }
 }
 
+Pagination.propTypes = {
+  totalRecords: PropTypes.number.isRequired,
+  pageLimit: PropTypes.number,
+  pageNeighbors: PropTypes.number,
+  OnPageChanged: PropTypes.func
+}

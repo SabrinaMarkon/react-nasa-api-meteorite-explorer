6c321ef2c5fc2a9b20b58f0b993740cc19273342
Sabrina Markon <phpsitescripts@outlook.com>
2019-05-31 03:54:54 -0600

    Forgot extends Component in Pagination component, so added.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 
-export default class Pagination {
+export default class Pagination extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
       currentPage: 1
     }
-    
+
   }
 }
+
